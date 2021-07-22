2a2be7f939aaed7d5d7472603e47944aa587cfb7
Damian <justice.damian96@gmail.com>
2021-07-23 07:27:28 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Damian is the champion';
